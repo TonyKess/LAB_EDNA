74c01cbd993b1d960c2717d1f06943038d5f7609
TonyKess <tonykess@gmail.com>
2022-06-21 14:53:06 -0300

    changed IDs

--- fastq_xz2gz_parallel.sh
+++ fastq_xz2gz_parallel.sh
@@ -2,15 +2,25 @@
 #SBATCH --time=12:00:00
 #SBATCH --account=def-ibradbur
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=tonykess@gmail.com
+#SBATCH --mail-user=samantha.crowley@dal.ca
 #SBATCH --nodes=1
 #SBATCH --mem=0
 #SBATCH --cpus-per-task=32
 #SBATCH --kill-on-invalid=yes
 #SBATCH --job-name=fastq_xz2gz_parallel.sh
 
+#get parameters file
+source EDNA_params.tsv
 
+#change directory
+cd $projdir
+
+#export source variables to keep parallel happy
+export projdir
+
+#make a list of fastq.xz names
 ls *xz | sed 's/.xz//' > XZfilelist
 
+# read in the list, pass to parallel, decompress with xz and recompress with gzip
 cat XZfilelist | parallel --jobs 32 ' xz -c -d {}.xz | gzip {}.gz '
 
